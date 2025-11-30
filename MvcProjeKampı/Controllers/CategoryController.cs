383441b8aa680030364341f8d6a93ef869c5eb68
YİĞİT ÖRÜCÜ <orucuyigit@gmail.com>
2025-11-30 18:39:55 +0300

    Added CategoryController and View

---
+++ MvcProjeKampı/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concreate;
+using BusinessLayer.Valitadions;
+using DataAccessLayer.EntityFramawork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampı.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController()
+        {
+            _categoryService = new CategoryManager(new EFCategoryDal(), new CategoryValitadions());
+        }
+        public ActionResult Index()
+        {
+            var values = _categoryService.TGetList();
+            return View(values);
+        }
+    }
+}
